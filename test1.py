ed5eb0d5f7eb99e449b121d7a0f1b39e6176c4b9
Tbuffon <389387731@qq.com>
2023-05-29 09:50:46 +0800

    first framework

---
+++ test1.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def crawl_page(url):
+    # Send a request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content using Beautiful Soup
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+
+        # Process the parsed HTML and extract the desired information
+        # (This will depend on the structure of the webpage you're crawling)
+        
+        # Example: print all the text inside paragraph tags
+        for p in soup.find_all('p'):
+            print(p.get_text())
+    else:
+        print(f"Failed to fetch the page: {url}")
+
+# Example usage: crawl a specific URL
+crawl_page('https://edfjzxt.xmu.edu.cn/website/page/Information/xxgk.html')
