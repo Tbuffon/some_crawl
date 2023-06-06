540e1ae24d4c9733ac0bb281e34be00f29d0d658
Tbuffon <389387731@qq.com>
2023-06-06 16:17:35 +0800

    test

--- CLSI网页抓取与表格转换/crawl.py
+++ CLSI网页抓取与表格转换/crawl.py
@@ -22,47 +22,76 @@ table_1a.click()
 
 browser.implicitly_wait(3)
 
-# title = browser.find_element(By.XPATH, '//*[@id="CLSI M100 ED33:2023 TABLE 1A"]/h1/b/span').text
-# sup = browser.find_element(By.XPATH, '//*[@id="CLSI M100 ED33:2023 TABLE 1A"]/h1/b/span/sup').text
-# print(sup)
-# if title.endswith(sup):
-#     title = title.rstrip(sup)
-# title = title.split('.')
 
-# 找到表格元素
-table = browser.find_element(By.XPATH, '//*[@id="CLSI M100 ED33:2023 TABLE 1A [Table Data]"]/div/table/tbody')
 
-# 获取表格所有行
-rows = table.find_elements(By.TAG_NAME, 'tr')
+
 
 # # 初始化数据列表
-data = []
+data = {
+ '抗生素':[],
+ '抗生素上标':[],
+ '抗生素说明':[],
+ '分级':[],
+ '分级说明':[],
+ '尿检':[],
+ '所在表格':[],
+ '表格名称':[],
+ '表格名称上标':[]}
 
-# # 遍历表格行,获取每个单元格的数据
-for row in rows:
-    # 获取行中的所有单元格
-    cols = row.find_elements(By.TAG_NAME, 'td') 
-    # 初始化单行的数据
-    row_data = []
-    # 遍历单元格
-    for col in cols:
-        # 获取单元格内容
-        text = col.text
-        # 添加到单行数据中
-        row_data.append(text)
-    # 添加单行数据到总的数据中
-    data.append(row_data)
 
-# 关闭浏览器    
-browser.close()
+title = browser.find_element(By.XPATH, '//*[@id="CLSI M100 ED33:2023 TABLE 1A"]/h1/b/span')
+sup = title.find_element(By.TAG_NAME, 'sup').text
+print(sup)
+if title.text.endswith(sup):
+    title = title.text.rstrip(sup)
+title = title.split('.')
+print(title)
+data['所在表格'].append(title[0])
+data['表格名称上标'].append(sup)
+data['表格名称'].append(title[1])
+
+data['抗生素'].append(None)
+data['抗生素上标'].append(None)
+data['抗生素说明'].append(None)
+data['分级'].append(None)
+data['分级说明'].append(None)
+data['尿检'].append(None)
+
+
+
+# # 找到表格元素
+# table = browser.find_element(By.XPATH, '//*[@id="CLSI M100 ED33:2023 TABLE 1A [Table Data]"]/div/table/tbody')
+
+# # 获取表格所有行
+# rows = table.find_elements(By.TAG_NAME, 'tr')
+
+# # # 遍历表格行,获取每个单元格的数据
+# for row in rows:
+#     # 获取行中的所有单元格
+#     cols = row.find_elements(By.TAG_NAME, 'td') 
+#     # 初始化单行的数据
+#     row_data = []
+#     # 遍历单元格
+#     for col in cols:
+#         # 获取单元格内容
+#         text = col.text
+#         # 添加到单行数据中
+#         row_data.append(text)
+#     # 添加单行数据到总的数据中
+#     data.append(row_data)
+
+# # 关闭浏览器    
+# browser.close()
 
 # 使用pandas转换为DataFrame
 df = pd.DataFrame(data)
 
-# 将行索引设置为列
-df = df.transpose()
+# # 将行索引设置为列
+# df = df.transpose()
 
-df.to_csv("table", sep='\t', encoding='utf-8', index=False)
+# # df.to_csv("table", sep='\t', encoding='utf-8', index=False)
+df.to_csv("table.csv", encoding='utf-8-sig', index=False)
+
 
 # print(browser.current_url)
 
@@ -70,16 +99,7 @@ df.to_csv("table", sep='\t', encoding='utf-8', index=False)
 #             f.write(str(browser.page_source))
 
 
-# data = {
-#  '抗生素':[],
-#  '抗生素上标':[],
-#  '抗生素说明':[],
-#  '分级':[],
-#  '分级说明':[],
-#  '尿检':[],
-#  '所在表格':[],
-#  '表格名称':[],
-#  '表格名称上标':[]}
+
 # df = pd.DataFrame(data)
 # filename = 'my_table.csv'
 # df.to_csv(filename, sep='\t', encoding='utf-8', index=False)
